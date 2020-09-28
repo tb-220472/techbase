334da0d126b9b7263fb78cffd79b5116b09886cd
tb-220472 <71576509+tb-220472@users.noreply.github.com>
2020-09-28 23:57:27 +0900

    Update mypage1.php

--- mypage1.php
+++ mypage1.php
@@ -24,6 +24,44 @@ $login_user = $_SESSION['login_user'];
        <h2>マイページ</h2>
     <p>ログインユーザ：<?php echo h($login_user['name'])?></p>
     <p>メールアドレス：<?php echo h($login_user['email'])?></p>
+    
+    <h1>しりとり掲示板</h1>
+<p>みんなでしりとり。</p>
+<p>名前・コメント・パスワードの入力は必須です。編集・削除の際にパスワードを使用します。<br></p>
+<p>編集モードでは、パスワード入力は必要ありません。<br></p>
+<h2><p>投稿フォーム</p></h2>
+<form action="mission6-2.php" method="post">
+<input type="text" name="name" placeholder="名前入力" value=<?php if(isset($newname)){echo $newname;}?>> 
+<input type="text" name="comment" placeholder="コメント入力" value=<?php if(isset($newcomment)){echo $newcomment;}?>>
+<input type="text" name="password" placeholder="新規パスワード設定"> 
+<input type="text" name="edit" value=<?php if(isset($newnumber)){echo $newnumber;}?>><!--隠し目印-->
+<input type="submit" name="submit"value="送信">
+<h2><p>削除フォーム</p></h2>
+<input type="number" name="delatenumber" placeholder="削除番号入力">
+<input type="text" name="delatepassword" placeholder="既定のパスワード入力"> 
+<input type="submit" name="delatesubmit"value="削除">
+<h2><p>編集フォーム</p></h2>
+<input type="number" name="editnumber" placeholder="編集番号入力">
+<input type="text" name="editpassword" placeholder="既定のパスワード入力"> 
+<input type="submit" name="editsubmit"value="編集">
+</form>
+<hr>
+    <?php
+    require_once './dbconnect.php';
+    $sql = 'SELECT * FROM blog3';
+	$stmt = $dbh->query($sql);
+	$results = $stmt->fetchAll();
+	foreach ($results as $row){
+		//$rowの中にはテーブルのカラム名が入る
+		echo $row['id'].',';
+		echo $row['name'].',';
+        echo $row['comment'].',';
+        echo $row['date'].'<br>';
+	echo "<hr>";
+	}
+    ?>
+    
+<hr>   
     <form action="logout.php" method="POST">
     <input type="submit" name="logout" value="ログアウト">
     </form>
